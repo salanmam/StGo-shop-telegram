9aed8bb271c0e56044cf97d742040b86ef2ccee1
BehR <125330779+salanmam@users.noreply.github.com>
2023-11-19 00:47:06 +0100

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,6 @@ function bot($method,$datas=[]){
     $ch = curl_init();
     curl_setopt($ch,CURLOPT_URL,$url);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
     $res = curl_exec($ch);
     if(curl_error($ch)){
         var_dump(curl_error($en));
